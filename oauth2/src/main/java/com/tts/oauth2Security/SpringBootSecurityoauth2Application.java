4b790cdd63d0eff2f2aae57334a0ebfa9ba1b313
Echu Seifu <79155072+Echuseifu@users.noreply.github.com>
2021-05-18 14:59:45 -0400

    Add files via upload

---
+++ oauth2/src/main/java/com/tts/oauth2Security/SpringBootSecurityoauth2Application.java
@@ -0,0 +1,13 @@
+package com.tts.oauth2Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityoauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityoauth2Application.class, args);
+	}
+
+}
---
+++ oauth2/src/test/java/com/tts/oauth2Security/SpringBootSecurityoauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.oauth2Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityoauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
